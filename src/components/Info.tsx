c7604380375759e052d5d3df57877f56682c48d3
Turtle Crab <sk.and.sk@gmail.com>
2024-03-31 23:47:58 +0300

    update info component

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,6 +1,21 @@
 export function Info() {
   return (
     <div>
+      <h2>Info</h2>
+      <ul>
+        <li>
+          <code>1-4</code> to change color
+        </li>
+        <li>
+          <code>Alt</code> for quick eye dropper
+        </li>
+        <li>
+          <code>Shift</code> with pencil tool to draw a line
+        </li>
+        <li>
+          <code>Middle mouse button</code> for quick hand tool
+        </li>
+      </ul>
       <h2>Github</h2>
       <a href="https://github.com/turtlecrab/GBSprite" target="_blank">
         turtlecrab/GBSprite
